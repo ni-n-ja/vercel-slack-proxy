cd85fd6d42fb85738f02c37632541058115609f7
ni_n_ja <rampage4extrm@gmail.com>
2021-03-07 03:17:57 +0900

    add files

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  jsxBracketSameLine: true,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 200,
+  tabWidth: 2,
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const main = () => {
+  console.log('!');
+  console.log('!');
+};
